e46940d958108998e792672ef113db1ea737feb8
Shan O <shanfineart@icloud.com>
2024-10-08 01:24:10 +0000

    Create-Bucket adjustment

--- s3/powershell-scripts/Create-Bucket.ps1
+++ s3/powershell-scripts/Create-Bucket.ps1
@@ -3,7 +3,18 @@ $region = "us-east-1"
 
 $bucketName = Read-Host -Prompt 'Enter the S3 bucket name'
 
-New-S3Bucket -BucketName $bucketName -Region $region
+function BucketExists {
+    $bucket = Get-S3BUcket -BucketName $bucket -ErrorAction SilentlyContinue
+    return $null -ne $bucket 
+}
+
+if (-not (BucketExists)) {
+    Write-Host "Bucket doesn't exist"
+    New-S3Bucket -BucketName $bucketName -Region $region
+}
+else {
+    $bucketName = Read-Host -Prompt "Bucket exists. Choose another bucket name."
+}
 
 Write-Host 'AWS Region: $region'
 Write-Host 'S3 Bucket: $bucketName'
